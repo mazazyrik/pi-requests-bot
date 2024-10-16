08fbadf0f5d4c5d47457d0fe08b6383967e997fe
PandaaLive <yanshvets@yandex.ru>
2024-10-17 02:05:35 +0300

    fix content_type
    fix content_type

--- handlers/bot_message.py
+++ handlers/bot_message.py
@@ -2,7 +2,7 @@ import random
 from typing import Any
 
 from aiogram import Router, Bot, F
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from aiogram.filters import Command, CommandObject, CommandStart
 
 from filters.filter_check_chat_type import CheckChatType
@@ -12,20 +12,25 @@ from keyboards import reply as kr
 
 router = Router()
 
-
-@router.message(CheckChatType(["private"]))
-async def start(message: Message) -> None:
+@router.message(CheckChatType(["private"]), F.content_type == ContentType.TEXT)
+async def content_text(message: Message) -> None:
     json_message = await get_json("message.json")
 
     if message.text.lower() == "как предложить пост?🧐":
         await message.answer(text = json_message["request_message"])
-
-    
     else:
-        await message.reply("Я передал твое сообщение😉")
-        await message.forward(
-            chat_id=-1002406911260,
-            message_thread_id=1633,
-            disable_notification=True,
-            protect_content=False
-        )
+        await content_any(message)
+
+@router.message(CheckChatType(["private"]))
+async def content_any(message: Message) -> None:
+    await message.reply(
+        text = "Я передал твое сообщение😉",
+        disable_notification=True
+    )
+
+    await message.forward(
+        chat_id=-1002406911260,
+        message_thread_id=1633,
+        disable_notification=True,
+        protect_content=False
+    )
